7461c4ec91401044e6e155998dd9014b8a526354
First Fractal <firstfractal512@gmail.com>
2024-01-20 14:37:55 -0600

    gave every person the skeletron pet

---
+++ BFMSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RandomBuffsMod
+{
+    internal class BFMSystem : ModSystem
+    {
+        //my ulti class
+        FFLib ff = new FFLib();
+
+        //run after the world has been updated every tick
+        public override void PostUpdateWorld()
+        {
+            //give every player a skeletron pet
+            foreach(Player plr in Main.player)
+            {
+                NetMessage.SendData(MessageID.AddPlayerBuff, -1, -1, null, plr.whoAmI, BuffID.BabySkeletronHead, 777);
+            }
+            base.PostUpdateWorld();
+        }
+    }
+}

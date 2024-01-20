c78241ab26858243938ab56680cfb44fdbf61668
First Fractal <firstfractal512@gmail.com>
2024-01-20 14:59:23 -0600

    added in a cooldown and make it not go off when the players are not actives

--- BFMSystem.cs
+++ BFMSystem.cs
@@ -8,7 +8,11 @@ namespace RandomBuffsMod
     internal class BFMSystem : ModSystem
     {
         //my libary class
-        FFLib ff = new FFLib();
+        static FFLib ff = new FFLib();
+
+        //track the cooldown for the random biff
+        int cooldownMax = ff.TimeToTick(5);
+        int cooldown = 0;
 
         //get a random buff ID
         public int randomBuffID = Main.rand.Next(1, BuffLoader.BuffCount);
@@ -16,12 +20,44 @@ namespace RandomBuffsMod
         //run after the world has been updated every tick
         public override void PostUpdateWorld()
         {
-            //give every player a random buff
-            foreach(Player plr in Main.player)
+            //flag to track if someone is active in the world
+            bool activePlayer = false;
+
+            //iter through each player to see if someone is active
+            foreach(Player player in Main.player)
             {
-                randomBuffID = Main.rand.Next(1, BuffLoader.BuffCount);
-                NetMessage.SendData(MessageID.AddPlayerBuff, -1, -1, null, plr.whoAmI, randomBuffID, 777);
+                if (player.active == true)
+                {
+                    activePlayer = true;
+                    break;
+                }
             }
+
+            //if a player is active, then start the cooldown
+            if (activePlayer)
+            {
+                //is the cooldown done yet
+                if (cooldown > 0)
+                {
+                    //decrease the cooldown
+                    cooldown--;
+                    Console.WriteLine(cooldown);
+                }
+                else
+                {
+                    //get a random buff
+                    randomBuffID = Main.rand.Next(1, BuffLoader.BuffCount);
+
+                    foreach (Player plr in Main.player)
+                    {
+                        //give the player the random buff
+                        NetMessage.SendData(MessageID.AddPlayerBuff, -1, -1, null, plr.whoAmI, randomBuffID, ff.TimeToTick(5));
+                    }
+                    cooldown = cooldownMax;
+                }
+            }
+            
+
             base.PostUpdateWorld();
         }
     }

726a4a3334f93aee60756efe3afbac0ae34b5162
First Fractal <firstfractal512@gmail.com>
2024-01-23 20:40:04 -0600

    prevent the mod from removing potion and mana sickness

--- RBMGlobalBuff.cs
+++ RBMGlobalBuff.cs
@@ -20,29 +20,38 @@ namespace RandomBuffsMod
             //disable the long expert mode debuff
             BuffID.Sets.LongerExpertDebuff[type] = false;
 
-            //put the timer on all debuffs
+            //put the timer on all buffs
             Array.Fill(Main.buffNoTimeDisplay, false);
             //Array.Fill(Main.vanityPet, false);
 
             //if the player only wants one buff and the current buff not the player buff, then remove it
             if (RBMConfig.Instance.onlyOneBuff && type != player.GetModPlayer<RBMPlayer>().randomBuffID)
             {
-                player.ClearBuff(type);
+                //don't get rid of potion and mana sickness
+                if (type != BuffID.PotionSickness && type != BuffID.ManaSickness) 
+                { 
+                    player.ClearBuff(type);
+                }
             }
 
             //do vanilla functions
             base.Update(type, player, ref buffIndex);
         }
 
+        //add in text to the tooltip of the buff to say the source of the mod
         public override void ModifyBuffText(int type, ref string buffName, ref string tip, ref int rare)
         {
             string buffSource = "Vanilla";
 
+            //if the buff ID is above the vanilla buff count, then its modded
             if (type > BuffID.Count)
             {
                 buffSource = "Modded";
             }
+            //format the text
             string buff = "(" + buffSource + " Buff)";
+
+            //color code the text and apply it to the tooltip
             tip += "\n[c/327DFF:" + buff + "]";
 
             base.ModifyBuffText(type, ref buffName, ref tip, ref rare);
